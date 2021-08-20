ece86019ef214f85b01cdba28733539fc185c4e5
enock <enckkipronoh@gmail.com>
2021-08-20 17:23:16 +0300

    adding another object attribute to the quote blueprint

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import {Quote} from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[]= [
-    {id:1, name:'No matter where you are from your dreams are valid.'},
-    {id:2, name:'Change the world by being yourself'},
-    {id:3, name:'Every moment is a fresh beginning.'},
-    {id:4, name:'Never regret anything that made you smile.'},
-    {id:5, name:'No human is limited. '},
-    {id:6, name:'Simplicity is the ultimate sophistication.'},
+    {id:1, name:'Lupita Nyongo', description:'No matter where you are from your dreams are valid.'},
+    {id:2, name:' T.S Eliot', description:'Change the world by being yourself'},
+    {id:3, name:'Mark Twain',description:'Every moment is a fresh beginning.'},
+    {id:4, name:'Amy Poehler',description:'Never regret anything that made you smile.'},
+    {id:5, name:'Eliud Kipchoge',description:'No human is limited. '},
+    {id:6, name:'Leonardo da Vinci',description:'Simplicity is the ultimate sophistication.'},
 
   ];
   constructor() { }
