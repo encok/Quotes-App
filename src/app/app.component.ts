dd44362122c7401b99d520c868a4065ddec4beba
enock <enckkipronoh@gmail.com>
2021-08-20 17:15:12 +0300

    adding the logic to the the new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,13 @@ import {Quote} from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: Quote[]= [
-    {id:1, name:'No matter where you are from your dreams are valid.'},
-    {id:2, name:'Change the world by being yourself'},
-    {id:3, name:'Every moment is a fresh beginning.'},
-    {id:4, name:'Never regret anything that made you smile.'},
-    {id:5, name:'No human is limited. '},
-    {id:6, name:'Simplicity is the ultimate sophistication.'},
+  // quotes: Quote[]= [
+  //   {id:1, name:'No matter where you are from your dreams are valid.'},
+  //   {id:2, name:'Change the world by being yourself'},
+  //   {id:3, name:'Every moment is a fresh beginning.'},
+  //   {id:4, name:'Never regret anything that made you smile.'},
+  //   {id:5, name:'No human is limited. '},
+  //   {id:6, name:'Simplicity is the ultimate sophistication.'},
 
-  ];
+  // ];
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
+
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[]= [
+    {id:1, name:'No matter where you are from your dreams are valid.'},
+    {id:2, name:'Change the world by being yourself'},
+    {id:3, name:'Every moment is a fresh beginning.'},
+    {id:4, name:'Never regret anything that made you smile.'},
+    {id:5, name:'No human is limited. '},
+    {id:6, name:'Simplicity is the ultimate sophistication.'},
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
