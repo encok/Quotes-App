66ff65f64681b2bf6596361071e36c40bc9d140c
enock <enckkipronoh@gmail.com>
2021-08-20 17:06:56 +0300

    adding directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Quote} from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quotes: Quote[]= [
+    {id:1, name:'No matter where you are from your dreams are valid.'},
+    {id:2, name:'Change the world by being yourself'},
+    {id:3, name:'Every moment is a fresh beginning.'},
+    {id:4, name:'Never regret anything that made you smile.'},
+    {id:5, name:'No human is limited. '},
+    {id:6, name:'Simplicity is the ultimate sophistication.'},
+
+  ];
 }
